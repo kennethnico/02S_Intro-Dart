cc11bd9c09aa30cd302e3e756a17ae9e5fc4fd2d
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-11 11:38:27 -0600

    Lección 2 Dart FH

--- 02_var_types.dart
+++ 02_var_types.dart
@@ -1,3 +1,15 @@
 void main(){
-  final String pokemon ="";
+  final String pokemon ="Ditto";
+  final int hp = 100;
+  final bool isAlive = true; //Null safety
+  final List<String> abilities = ["impostor"];
+  final sprites = <String>['ditto/front.png','ditto/back.png'];
+
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  """);
 }
