175047c25456821c4f9bfb3fb25e7c1986e442b6
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-11 11:31:30 -0600

    Initial commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  //var myName = "Kenneth";
+  //final myName = "Kenneth";
+  //late final myName = "Kenneth";
+  //const myName = "Kenneth";
+  String myName = "Kenneth";
+  print('Hola $myName'); //Interpolación de Strings
+  print('Hola ${myName.toUpperCase()}'); //Interpolación de Strings
+  print('${ 1 + 1}'); //Interpolación de Strings
+}
