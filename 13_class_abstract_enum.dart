7c7842a7143833caa05960e68c50e70d750eb7b4
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-12 13:43:25 -0600

    Lección 13 Dart FH

---
+++ 13_class_abstract_enum.dart
@@ -0,0 +1,15 @@
+//Clases abstractas y enumeraciones
+/*
+* Uso real y Ejemplos
+* En general una clase abstracta es un molde que funciona para crear otros moldes.
+* El molde inicial solo funciona para hacer otros moldes
+* Una clase abstracta no se puede instanciar por si misma
+* Su objetivo es aplicar diferentes patrones de arquitectura*/
+void main(){
+  //final windPlant = EnergyPlant();
+}
+abstract class EnergyPlant{
+  double energyLeft;
+  String type; //Nuclear, Viento y Agua
+
+}
