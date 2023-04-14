9fbf57b3a1dd57987f841ebde868df43502b1ef8
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-14 10:39:11 -0600

    Lección 13 Dart FH

--- 13_class_abstract_enum.dart
+++ 13_class_abstract_enum.dart
@@ -6,10 +6,16 @@
 * Una clase abstracta no se puede instanciar por si misma
 * Su objetivo es aplicar diferentes patrones de arquitectura*/
 void main(){
-  //final windPlant = EnergyPlant();
+  //final windPlant = EnergyPlant(); //No se puede instanciar una clase abstracta
 }
+enum PlanType { nuclear, wind, water}
 abstract class EnergyPlant{
-  double energyLeft;
-  String type; //Nuclear, Viento y Agua
-
+  double energyLeft; //Energía restante
+  PlanType type; //Nuclear, Wind or Water
+  //Constructor
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type
+  });
+  void consumeEnergy(double amount);
 }
