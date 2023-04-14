fd0f6d89a9b9b2b85675ae1c7d4e582d6eb47b48
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-14 13:19:36 -0600

    Lección 21 Dart FH

---
+++ 21_async*_await.dart
@@ -0,0 +1,16 @@
+/*
+* Simboliza que la función devuelve un Stream
+* */
+void main (){
+  emitNumbers()
+      .listen((int value) {
+    print('Stream value: $value');
+  });
+}
+Stream<int> emitNumbers()async*{
+  final valuesToEmit =[1,2,3,4,5];
+  for(int i in valuesToEmit){
+    await Future.delayed(const Duration(seconds: 1));
+    yield i; //Ceder un valor
+  }
+}
