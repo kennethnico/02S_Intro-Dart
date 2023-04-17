88b63c8fc386af5836702d4963a118a735775859
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-04-17 12:28:39 -0600

    Lección 21 Dart FH

--- 21_async*_await.dart
+++ 21_async*_await.dart
@@ -8,7 +8,7 @@ void main (){
   });
 }
 Stream<int> emitNumbers()async*{
-  final valuesToEmit =[1,2,3,4,5];
+  final valuesToEmit =[1,2,3,4,5,6,7];
   for(int i in valuesToEmit){
     await Future.delayed(const Duration(seconds: 1));
     yield i; //Ceder un valor
